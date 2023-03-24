1d11373b21aa78b3e8d89890bbd23fe0a51105c6
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-03-24 20:32:52 +0300

    Task 16 : Implementation of Query Controller Process

---
+++ bank-account/account.query/src/main/java/com/bankcqrsexample/account/query/api/controllers/AccountLookupController.java
@@ -0,0 +1,109 @@
+package com.bankcqrsexample.account.query.api.controllers;
+
+import com.bankcqrsexample.account.query.api.dto.AccountLookupResponse;
+import com.bankcqrsexample.account.query.api.dto.EqualityType;
+import com.bankcqrsexample.account.query.api.queries.FindAccountByHolderQuery;
+import com.bankcqrsexample.account.query.api.queries.FindAccountByIdQuery;
+import com.bankcqrsexample.account.query.api.queries.FindAccountWithBalanceQuery;
+import com.bankcqrsexample.account.query.api.queries.FindAllAccountsQuery;
+import com.bankcqrsexample.account.query.domain.BankAccount;
+import com.bankcqrsexample.cqrs.core.infrastructure.QueryDispatcher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(path = "/api/v1/bankAccountLookup")
+@RequiredArgsConstructor
+public class AccountLookupController {
+
+    private final Logger logger = Logger.getLogger(AccountLookupController.class.getName());
+
+    private final QueryDispatcher queryDispatcher;
+
+    @GetMapping(path = "/")
+    public ResponseEntity<AccountLookupResponse> getAllAccounts() {
+        try {
+            List<BankAccount> accounts = queryDispatcher.send(new FindAllAccountsQuery());
+            if (accounts == null || accounts.size() == 0) {
+                return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+            }
+            var response = AccountLookupResponse.builder()
+                    .accounts(accounts)
+                    .message(MessageFormat.format("Successfully returned {0} bank account(s)!", accounts.size()))
+                    .build();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            var safeErrorMessage = "Failed to complete get all accounts request!";
+            logger.log(Level.SEVERE, safeErrorMessage, e);
+            return new ResponseEntity<>(new AccountLookupResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/byId/{id}")
+    public ResponseEntity<AccountLookupResponse> getAccountById(@PathVariable(value = "id") String id) {
+        try {
+            List<BankAccount> accounts = queryDispatcher.send(new FindAccountByIdQuery(id));
+            if (accounts == null || accounts.size() == 0) {
+                return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+            }
+            var response = AccountLookupResponse.builder()
+                    .accounts(accounts)
+                    .message("Successfully returned bank account!")
+                    .build();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            var safeErrorMessage = "Failed to complete get accounts by ID request!";
+            logger.log(Level.SEVERE, safeErrorMessage, e);
+            return new ResponseEntity<>(new AccountLookupResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/byHolder/{accountHolder}")
+    public ResponseEntity<AccountLookupResponse> getAccountByHolder(@PathVariable(value = "accountHolder") String accountHolder) {
+        try {
+            List<BankAccount> accounts = queryDispatcher.send(new FindAccountByHolderQuery(accountHolder));
+            if (accounts == null || accounts.size() == 0) {
+                return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+            }
+            var response = AccountLookupResponse.builder()
+                    .accounts(accounts)
+                    .message("Successfully returned bank account!")
+                    .build();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            var safeErrorMessage = "Failed to complete get accounts by holder request!";
+            logger.log(Level.SEVERE, safeErrorMessage, e);
+            return new ResponseEntity<>(new AccountLookupResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/withBalance/{equalityType}/{balance}")
+    public ResponseEntity<AccountLookupResponse> getAccountWithBalance(@PathVariable(value = "equalityType") EqualityType equalityType,
+                                                                       @PathVariable(value = "balance") double balance) {
+        try {
+            List<BankAccount> accounts = queryDispatcher.send(new FindAccountWithBalanceQuery(equalityType, balance));
+            if (accounts == null || accounts.size() == 0) {
+                return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+            }
+            var response = AccountLookupResponse.builder()
+                    .accounts(accounts)
+                    .message(MessageFormat.format("Successfully returned {0} bank account(s)!", accounts.size()))
+                    .build();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            var safeErrorMessage = "Failed to complete get accounts with balance request!";
+            logger.log(Level.SEVERE, safeErrorMessage, e);
+            return new ResponseEntity<>(new AccountLookupResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
