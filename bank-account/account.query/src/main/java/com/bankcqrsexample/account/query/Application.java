4c0846e6edbca231bbf0ab48746d1a5fb18e43a4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-03-24 20:20:13 +0300

    Task 14 : Implementation of Query Handling Process

--- bank-account/account.query/src/main/java/com/bankcqrsexample/account/query/Application.java
+++ bank-account/account.query/src/main/java/com/bankcqrsexample/account/query/Application.java
@@ -1,13 +1,29 @@
 package com.bankcqrsexample.account.query;
 
+import com.bankcqrsexample.account.query.api.queries.*;
+import com.bankcqrsexample.cqrs.core.infrastructure.QueryDispatcher;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class Application {
+
+	private final QueryDispatcher queryDispatcher;
+
+	private final QueryHandler queryHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@PostConstruct
+	public void registerHandlers() {
+		queryDispatcher.registerHandler(FindAllAccountsQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByIdQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByHolderQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountWithBalanceQuery.class, queryHandler::handle);
+	}
 }
--- bank-account/account.query/src/main/java/com/bankcqrsexample/account/query/infrastructure/consumers/AccountEventConsumer.java
+++ bank-account/account.query/src/main/java/com/bankcqrsexample/account/query/infrastructure/consumers/AccountEventConsumer.java
@@ -4,21 +4,30 @@ import com.bankcqrsexample.account.common.events.AccountClosedEvent;
 import com.bankcqrsexample.account.common.events.AccountOpenedEvent;
 import com.bankcqrsexample.account.common.events.FundsDepositedEvent;
 import com.bankcqrsexample.account.common.events.FundsWithdrawnEvent;
+import com.bankcqrsexample.account.query.api.controllers.AccountLookupController;
 import com.bankcqrsexample.account.query.infrastructure.handlers.EventHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
 @RequiredArgsConstructor
 public class AccountEventConsumer implements EventConsumer {
+
+    private final Logger logger = Logger.getLogger(AccountLookupController.class.getName());
 
     private final EventHandler eventHandler;
 
     @KafkaListener(topics = "AccountOpenedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
     public void consume(AccountOpenedEvent event, Acknowledgment ack) {
+
+        logger.log(Level.INFO, "AccountEventConsumer is working with AccountOpenedEvent");
+
         this.eventHandler.on(event);
         ack.acknowledge();
     }
@@ -26,6 +35,9 @@ public class AccountEventConsumer implements EventConsumer {
     @KafkaListener(topics = "FundsDepositedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
     public void consume(FundsDepositedEvent event, Acknowledgment ack) {
+
+        logger.log(Level.INFO, "AccountEventConsumer is working with FundsDepositedEvent");
+
         this.eventHandler.on(event);
         ack.acknowledge();
     }
@@ -33,6 +45,9 @@ public class AccountEventConsumer implements EventConsumer {
     @KafkaListener(topics = "FundsWithdrawnEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
     public void consume(FundsWithdrawnEvent event, Acknowledgment ack) {
+
+        logger.log(Level.INFO, "AccountEventConsumer is working with FundsWithdrawnEvent");
+
         this.eventHandler.on(event);
         ack.acknowledge();
     }
@@ -40,6 +55,9 @@ public class AccountEventConsumer implements EventConsumer {
     @KafkaListener(topics = "AccountClosedEvent", groupId = "${spring.kafka.consumer.group-id}")
     @Override
     public void consume(AccountClosedEvent event, Acknowledgment ack) {
+
+        logger.log(Level.INFO, "AccountEventConsumer is working with AccountClosedEvent");
+
         this.eventHandler.on(event);
         ack.acknowledge();
     }
